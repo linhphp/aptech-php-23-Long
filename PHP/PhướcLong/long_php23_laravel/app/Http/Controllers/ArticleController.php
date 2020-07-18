fb079dfcde50c01744cde232af8db4b4b8d1474d
Longok <huynhphuoclong2@gmail.com>
2020-07-18 07:28:52 +0700

    kiem tra

---
+++ PHP/PhướcLong/long_php23_laravel/app/Http/Controllers/ArticleController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Article;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $articles = Article::all();
+        return view('articles.index',[
+            'articles'=>$articles
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+       return view('articles.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $title = $request->title;
+        $slug = Str::slug($title, '-');
+        $description = $request->description;
+        $content = $request->content;
+    
+        Article::create([
+            'title' => $title,
+            'slug' => $slug,
+            'description' => $description,
+            'content' => $content
+        ]);
+        return redirect()->route('articles.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($article)
+    {
+        $article = Article::find($article);
+        return view('articles.show',[
+            'article'=>$article
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = User::find($id);
+        return view('users.edit',
+        [
+            'user'=>$user
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
